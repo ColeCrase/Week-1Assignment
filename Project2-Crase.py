f496d67c93ba7b25ed693bdabd2d780d3dceb829
ColeCrase <86010417+ColeCrase@users.noreply.github.com>
2021-06-16 11:03:03 -0500

    Add files via upload
    These are my python files for the project assignment week 1

---
+++ Project2-Crase.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = "Cole Crase"
+>>> address = "501 NE St"
+>>> telephone = "999-171-7713"
+>>> print(name, address, telephone)
+Cole Crase 501 NE St 999-171-7713
+>>> 
---
+++ Project5-Crase.py
@@ -0,0 +1,4 @@
+base = int(input("Enter with base: "))
+height = int(input("Enter with height: "))
+area = .5 * base * height
+print("The area of a triangle is", area)
